fe0df5f04c49221ab20436e30d4377737bcc6bd0
Rajesh <agentic.engineer@gmail.com>
2025-05-12 21:48:48 -0500

    Draft-jotform version

--- config.js
+++
@@ -1,8 +0,0 @@
-// Authentication Configuration
-const AUTH_CONFIG = {
-    username: "testuser",  // Default user created by add_user.py
-    password: "testpass"   // Default password for testuser
-};
-
-// Export the configuration
-export default AUTH_CONFIG;
